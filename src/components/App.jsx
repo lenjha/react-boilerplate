c0f610e775509961a0bf057a70d600778a5145d3
Guest User <Guest@Epicodus-1E.local>
2018-03-05 11:22:52 -0800

    setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      This is a boilerplate.
+    </div>
+  );
+}
+
+export default App;
